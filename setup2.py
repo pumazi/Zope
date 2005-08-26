925222393eb6917c385cab208727b97982a70015
fdrake <fdrake@localhost>
2005-08-26 19:46:58 +0000

    snapshot current state of the zpkg build support for Zope 2.9; includes moving to newer versions of the Zope 3 and ZODB software
    svn path=/Zope/branches/zpkg-build-branch/; revision=38112

---
+++ setup2.py
@@ -0,0 +1,36 @@
+#############################################################################
+#
+# Copyright (c) 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os
+
+import zpkgsetup.package
+import zpkgsetup.publication
+import zpkgsetup.setup
+
+# Note that release.py must be able to recognize the VERSION line.
+VERSION = "2.9.0a42"
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+context = zpkgsetup.setup.SetupContext(
+    "Zope", VERSION, __file__)
+
+context.load_metadata(
+    os.path.join(here,
+                 zpkgsetup.publication.PUBLICATION_CONF))
+
+context.scan_collection("Zope", here, "release")
+context.walk_packages("lib/python")
+context.setup()
