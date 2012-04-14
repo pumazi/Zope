c7a2c358d5e25b9cd4da20c1ba0d262d76dd5f49
Michael Mulich <michael.mulich@gmail.com>
2012-04-14 11:04:48 -0400

    Restoring the ObjectManager's dependence on webdav's NullResource.
    Apparently this dependency runs really fucking deep. I've tagged it for
    later removal, after I've ripped out all the other cruft.

--- src/OFS/ObjectManager.py
+++ src/OFS/ObjectManager.py
@@ -47,6 +47,7 @@ from DateTime import DateTime
 from Persistence import Persistent
 from webdav.Collection import Collection
 from webdav.Lockable import ResourceLockedError
+from webdav.NullResource import NullResource
 from zExceptions import BadRequest
 from zope.interface import implements
 from zope.component.interfaces import ComponentLookupError
@@ -553,6 +554,14 @@ class ObjectManager(CopyContainer,
     def __getitem__(self, key):
         v=self._getOb(key, None)
         if v is not None: return v
+        # RRR zmi-killer
+        # XXX Grumble Grumble...
+        if hasattr(self, 'REQUEST'):
+            request=self.REQUEST
+            method=request.get('REQUEST_METHOD', 'GET')
+            if request.maybe_webdav_client and not method in ('GET', 'POST'):
+                return NullResource(self, key, request).__of__(self)
+        # /RRR zmi-killer
         raise KeyError, key
 
     def __setitem__(self, key, value):
